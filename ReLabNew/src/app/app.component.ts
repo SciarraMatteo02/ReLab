a5f6bc121a1907e8819b72c13d24e794b2768148
SciarraMatteo02 <sciarra.matteo@iisgalvanimi.edu.it>
2021-10-07 09:33:23 +0000

    fine 01.1 parte gatto

--- ReLabNew/src/app/app.component.ts
+++ ReLabNew/src/app/app.component.ts
@@ -9,16 +9,22 @@ export class AppComponent {
   title = 'server mappe';
   markerOptions: google.maps.MarkerOptions;
   markerOptions2: google.maps.MarkerOptions;
+  markerOptions3: google.maps.MarkerOptions;
+
   //Aggiungiamo latitudine e longitudine di un luogo
   center : any;
   position : any;
   label :string;
   circleOptions: { fillColor: string; }; 
   circleOptions2: { fillColor: string; };
+  position2: { lat: number; lng: number; };
+  position3: { lat: number; lng: number; };
   constructor()
   {
     this.center={lat: 45.506738, lng: 9.190766};
     this.position = this.center;
+    this.position2={lat: 45.507338, lng: 9.190766};
+    this.position3={lat: 45.506038, lng: 9.190766};
     this.label = "ciao";
     this.circleOptions = {fillColor : 'red'}
     this.circleOptions2 = {fillColor : 'red'}
@@ -31,11 +37,18 @@ export class AppComponent {
       this.markerOptions = {icon:iconData}
 
    let iconData2 : google.maps.Icon = {
-        url:'./assets/img/piccione.jpg',
-        scaledSize : new google.maps.Size(60,60)
+        url:'./assets/img/piccione.ico',
+        scaledSize : new google.maps.Size(110,110)
       }
     
         this.markerOptions2 = {icon:iconData2}
+
+    let iconData3 : google.maps.Icon = {
+        url:'./assets/img/kebab.ico',
+        scaledSize : new google.maps.Size(80,80)
+        }
+      
+          this.markerOptions3 = {icon:iconData3}
 
   }
   blue(){
